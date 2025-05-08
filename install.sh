d7e66b7c012ba03ae36ae271740d5624a06d02af
Miguel Barreto <miguelandres@users.noreply.github.com>
2025-05-08 15:46:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 ## Note this install script is only wor codespaces! Don't use elsewhere
 
 ./download.sh x86_64-unknown-linux-musl
-./dotfiles install-oh-my-zsh
 ./dotfiles install-homebrew
 ./dotfiles apply-config codespaces-work.yaml
 source ~/.zshrc
